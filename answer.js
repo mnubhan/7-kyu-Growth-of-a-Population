a1f1868dde1d791165a29b9dce2c344c5b4c2e64
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-09-09 23:46:27 +0100

    Update answer.js

--- answer.js
+++ answer.js
@@ -1,6 +1,12 @@
-const nbYear =function(p0, percent, aug, p){
+econst nbYear =function(p0, percent, aug, p){
   for(var year=0;p0<p;year++){
-    p0+=(p0*percent/100)+aug
+    p0+=Math.floor(p0*percent/100)+aug
   }
   return year
 }
+
+
+/* 
+var can be use outside it scope
+let can be use inside it scope
+ */
